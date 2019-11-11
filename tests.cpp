62d362c854ebec8dd6a361a1adab04b1964a6f93
Franck W <franck.rj.w@gmail.com>
2019-11-11 10:31:27 +0100

    Changements des tabulations en espaces.

--- tests.cpp
+++ tests.cpp
@@ -5,118 +5,118 @@
 
 std::string& function()
 {
-	static std::string lol;
-	return lol;
+    static std::string lol;
+    return lol;
 }
 
 std::string otherFunction()
 {
-	return std::string{};
+    return std::string{};
 }
 
 void lvalueFunction()
 {
 
-	std::string obj;
-	obj;            // lvalue
+    std::string obj;
+    obj;            // lvalue
 
-	std::string& function();
-	function();     // lvalue
+    std::string& function();
+    function();     // lvalue
 
-	std::string&& rvalRef = std::move(obj);
-	rvalRef;        // lvalue
+    std::string&& rvalRef = std::move(obj);
+    rvalRef;        // lvalue
 
 }
 
 void xvalueFunction()
 {
 
-	std::string obj;
-	std::move(obj);                 // xvalue
+    std::string obj;
+    std::move(obj);                 // xvalue
 
-	std::pair<int, int>{}.first;    // xvalue
+    std::pair<int, int>{}.first;    // xvalue
 
-	std::pair<int, int> pair;
-	std::move(pair).first;          // xvalue
+    std::pair<int, int> pair;
+    std::move(pair).first;          // xvalue
 
 }
 
 struct jsp
 {
-	void prvalueFunction()
-	{
+    void prvalueFunction()
+    {
 
-		std::string{};      // prvalue
+        std::string{};      // prvalue
 
-		std::string otherFunction();
-		otherFunction();    // prvalue
+        std::string otherFunction();
+        otherFunction();    // prvalue
 
-	}
+    }
 };
 
 class VerboseParameter
 {
 public:
-	VerboseParameter() { std::cout << "VerboseParameter default constructor." << std::endl; }
-	VerboseParameter(const VerboseParameter& other) { std::cout << "VerboseParameter copy constructor." << std::endl; }
-	VerboseParameter(VerboseParameter&& other) noexcept { std::cout << "VerboseParameter move constructor." << std::endl; }
+    VerboseParameter() { std::cout << "VerboseParameter default constructor." << std::endl; }
+    VerboseParameter(const VerboseParameter& other) { std::cout << "VerboseParameter copy constructor." << std::endl; }
+    VerboseParameter(VerboseParameter&& other) noexcept { std::cout << "VerboseParameter move constructor." << std::endl; }
 };
 
 class VerboseClass
 {
 public:
-	VerboseClass() { std::cout << "VerboseClass default constructor." << std::endl; }
-	VerboseClass(int newValue) : value{ newValue } { std::cout << "VerboseClass arg constructor. (" << value << ": " << this << ")" << std::endl; }
-	VerboseClass(const VerboseClass& other) { std::cout << "VerboseClass copy constructor." << std::endl; }
-	VerboseClass(VerboseClass&& other) noexcept {
-		if (other.value != 0)
-			std::cout << "VerboseClass move constructor. (" << other.value << ": " << this << ")" << std::endl;
-		else
-			std::cout << "VerboseClass move constructor." << std::endl;
-	}
-	VerboseClass(VerboseParameter&& other) { std::cout << "VerboseClass parameter move constructor." << std::endl; }
-	VerboseClass(const VerboseParameter& other) { std::cout << "VerboseClass parameter copy constructor." << std::endl; }
+    VerboseClass() { std::cout << "VerboseClass default constructor." << std::endl; }
+    VerboseClass(int newValue) : value{ newValue } { std::cout << "VerboseClass arg constructor. (" << value << ": " << this << ")" << std::endl; }
+    VerboseClass(const VerboseClass& other) { std::cout << "VerboseClass copy constructor." << std::endl; }
+    VerboseClass(VerboseClass&& other) noexcept {
+        if (other.value != 0)
+            std::cout << "VerboseClass move constructor. (" << other.value << ": " << this << ")" << std::endl;
+        else
+            std::cout << "VerboseClass move constructor." << std::endl;
+    }
+    VerboseClass(VerboseParameter&& other) { std::cout << "VerboseClass parameter move constructor." << std::endl; }
+    VerboseClass(const VerboseParameter& other) { std::cout << "VerboseClass parameter copy constructor." << std::endl; }
 
-	int value = 0;
+    int value = 0;
 };
 
 VerboseClass buildFromClass()
 {
-	VerboseClass other;
-	return other;
+    VerboseClass other;
+    return other;
 }
 
 VerboseClass buildFromParamter()
 {
-	VerboseParameter param;
-	return param;
+    VerboseParameter param;
+    return param;
 }
 
 VerboseClass iBuildStuff()
 {
-	return VerboseClass{};
+    return VerboseClass{};
 }
 
 void basicExemple()
 {
-	VerboseClass imStuff = iBuildStuff();
+    VerboseClass imStuff = iBuildStuff();
 }
 
 VerboseClass iBuildMoreStuff()
 {
-	VerboseClass notStuff{ 21 };
-	return VerboseClass{ 25 };
+    VerboseClass notStuff{ 21 };
+    return VerboseClass{ 25 };
 }
 
 void advancedExemple()
 {
-	VerboseClass alsoNotStuff{ 11 };
-	VerboseClass imStuff = iBuildMoreStuff();
-	VerboseClass stillNotStuff{ 12 };
+    VerboseClass alsoNotStuff{ 11 };
+    VerboseClass imStuff = iBuildMoreStuff();
+    VerboseClass stillNotStuff{ 12 };
 }
 
 int main()
 {
-	//basicExemple();
-	VerboseClass imStuff = iBuildStuff();
+    //basicExemple();
+    VerboseClass imStuff = iBuildStuff();
 }
